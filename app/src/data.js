6356f46d8a62280d7a230adbb5698ad73b1bfdca
vasokhachidze <87890790+vasokhachidze@users.noreply.github.com>
2023-02-15 20:42:21 +0400

    add data info

---
+++ app/src/data.js
@@ -0,0 +1,139 @@
+const jobs = [
+  {
+    title: "USA-Based Company Needs Front-End Developer",
+    per: "Hourly: $25-$40 - Intermediate - Est. Time: 3 to 6 months, Less than 30 hrs/week - Posted 47 minutes ago",
+    text: "Hello How are you? We are an ambitious well-funded USA-based digital start-ups in Pakistan. We're Looking for Front-End Developer to help our rapidly growing workload. This will be more of a long-term project requiring great commitment. Excellent written and verbal communication skills. Highly organized and able to manage multiple tasks simultaneously. Patience, understanding, and an ability to work with people from diverse backgrounds. Looking for a long term hire who can commit 20-30 hours per week. DO NOT APPLY UNTIL YOU'VE REVIEWED THE ENTIRE JOB DESCRIPTION & DETAILS By clicking Job Description Updated Section at the Web Page given below: https://urlzs.in/nfmsU Click at Job Description Section",
+    skills: "CSS HTML",
+    prop: "5 to 10",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M12.6 7.181V1.4c0-.771-.63-1.4-1.4-1.4H1.399C.63 0 0 .629 0 1.4v7c0 .771.63 1.4 1.399 1.4h4.918A3.85 3.85 0 1012.6 7.181zM11.2 1.4v1.401H1.399V1.4H11.2zm-4.477 7H1.399V4.901H11.2v1.548a3.84 3.84 0 00-1.051-.149 3.845 3.845 0 00-3.426 2.1zm5.527 2.917l-.934.933-1.167-1.166-1.167 1.166-.932-.933 1.166-1.167L8.05 8.983l.932-.933 1.167 1.168 1.167-1.168.934.934-1.168 1.167 1.168 1.166z"></path></svg>',
+    verifi: "Payment unverified",
+    spent: "$0",
+    stars: "0",
+    location: "pacistan",
+  },
+
+  {
+    title: "Help with intro to html/css",
+    per: "Fixed-price - Entry level - Est. Budget: $5 - Posted 12 hours ago",
+    text: "I need assistance with a coding assignment that involves basic html and css knowledge. Please send a proposal if you have expertise in html or css.",
+    skills: "CSS HTML",
+    prop: "50+",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment verified",
+    spent: "$40",
+    stars: "0",
+    location: "Puerto Rico",
+  },
+
+  {
+    title: "Frontend HTML Responsive Websites",
+    per: "Hourly: $5-$15 - Expert - Est. Time: Less than 1 month, Less than 30 hrs/week - Posted 20 hours ago",
+    text: "I need a landing page design in HTML SCSS with professional neat and clean code with W3C standards.",
+    skills:
+      "Bootstrap Responsive Design Query Website CSS 3 Sass Landing Page JavaScript HTML",
+    prop: "20 to 50",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment unverified",
+    spent: "$0",
+    stars: "0",
+    location: "pacistan",
+  },
+
+  {
+    title: "Front End Developer",
+    per: "Hourly: $7-$20 - Expert - Est. Time: More than 6 months, 30+ hrs/week - Posted 24 hours ago",
+    text: "Front End Developer Job Summary: We are seeking a skilled Front End Developer to join our Dutch Agency team and create Wordpress website and shopify, responsive, and user-friendly web applications. You will leverage tools such as Clickup, Figma, and AdobeXD to deliver exceptional user experiences. Key Responsibilities: Develop and maintain",
+    skills:
+      "Web Development Front-End Development Website WordPress CSS WordPress Development Shopify Shopify",
+    prop: "20 to 50",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment verified",
+    spent: "$300",
+    stars: "5",
+    location: "Netherlands",
+  },
+
+  {
+    title: "Experience working with web development",
+    per: "Hourly: $33-$45 - Intermediate - Est. Time: 3 to 6 months, Less than 30 hrs/week - Posted 11 hours ago",
+    text: "Bachelor's degree in Computer Science or a related field At least three years of experience working with web development Proficient knowledge of HTML, CSS, JavaScript, and other programming languages Ability to troubleshoot and debug code Excellent communication and collaboration skills Ability to work independently and as part of a team",
+    skills: "Shopify",
+    prop: "15 to 20",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment unverified",
+    spent: "0$",
+    location: "pacistan",
+  },
+
+  {
+    title:
+      "Build website for sports tipping that people can login, and register their tips",
+    per: "Fixed-price - Expert - Est. Budget: $2,000 - Posted 12 hours ago",
+    text: "We want a website like www.itipfooty.com.au/ - BUT only for the entry. People need to be able to register, login, and then we want a 'Knockout Comp' for National Rugby League competition. For the Knockout comp though, we want the ability to set criteria, such as you can only pick one team every 6 rounds.",
+    skills: "Web Development Web Application HTML5 Web Design  HTML",
+    prop: "50+",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment verified",
+    spent: "$10k+",
+    location: "Australia",
+  },
+
+  {
+    title: "PSD (or figma) to HTML/CSS/JS",
+    per: "Hourly: $10-$60 - Expert - Est. Time: Less than 1 month, Less than 30 hrs/week - Posted 7 hours ago",
+    text: "We need someone who is reliable, and can quickly convert PSD (and figma in some cases) to HTML/CSS/JS - Landing pages + more - INCLUDING responsive layouts (with or without frameworks) show 3 examples We are expecting quality like this: https://hopium.com/",
+    skills: "CSS CSS 3 Landing Page HTML5 HTML",
+    prop: "50+",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment verified",
+    spent: "$5k+",
+    location: "South Africa",
+  },
+
+  {
+    title: "Create Tailwind CSS + Next.JS templates",
+    per: "Fixed-price - Expert - Est. Budget: $10,000 - Posted 17 hours ago",
+    text: "I'm interested in creating something to Tailwind UI (https://tailwindui.com/), not a copy or anything like that but a set of templates that can be sold to developers. I'm a developer with 10+ years of experience, and love these sorts of template packs. So, I'm interested in a full project that would span several different websites. The goal for the templates would be to",
+    skills: "Front-End Development Next.js Tailwind CSS Web Design React",
+    prop: "50+",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment unverified",
+    spent: "0$",
+    location: "United States",
+  },
+
+  {
+    title: "Javascript - Page Loading Effect",
+    per: "Fixed-price - Intermediate - Est. Budget: $50 - Posted 11 hours ago",
+    text: "Hello, I would like your help creating a loading screen that we'll be displaying to customers once they click a button. It has an animated loading bar with text captions. See the example linked below of an example I'd like you to copy very closely. 1- clicks button with function attached 2- loading screen displays and loads, covering the screen 3- forward",
+    skills: "JavaScript HTML",
+    prop: "50+",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment verified",
+    spent: "$20k +",
+    location: "United Kingdom",
+  },
+
+  {
+    title: "Sanity.IO Back End Web Maintenance",
+    per: "Hourly - Intermediate - Est. Time: Less than 1 month, Less than 30 hrs/week - Posted 21 hours ago",
+    text: "Need someone fluent in SANITY.IO to help us maintain and update our website on a weekly basis! Will need to be able to change things in sanity, upload artworks , change product pages and also huge plus if you know how to use shopify backend!",
+    skills:
+      "Web Development Website CSS Elementor Next.js React HTML Bootstrap WordPress Search Engine Optimization Shopify Web Design JavaScript Tailwind CSS",
+    prop: "20 to 50",
+    vicon:
+      '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img"><path d="M13.72 7.03c.45-.56.34-1.39-.24-1.82l-.55-.41c-.34-.25-.53-.66-.51-1.08l.03-.68c.03-.72-.53-1.32-1.25-1.33h-.68c-.42 0-.81-.22-1.05-.57L9.11.57c-.39-.6-1.2-.75-1.79-.33l-.55.4c-.34.24-.79.3-1.18.15L4.95.55c-.67-.25-1.41.11-1.64.79l-.21.65c-.14.4-.46.71-.87.82l-.65.18C.89 3.19.5 3.92.71 4.6l.21.65c.13.41.04.85-.22 1.18l-.42.54c-.45.56-.34 1.39.24 1.81l.55.41c.34.25.53.66.51 1.08l-.03.68c-.03.72.54 1.32 1.25 1.33h.68c.42 0 .81.22 1.05.57l.37.57c.39.6 1.21.75 1.79.33l.55-.4c.34-.25.78-.31 1.18-.16l.64.24c.67.25 1.41-.1 1.64-.79l.21-.65c.13-.4.45-.71.86-.82l.65-.17c.69-.19 1.09-.92.87-1.61l-.21-.65c-.13-.4-.05-.85.22-1.18l.42-.53zM6.06 9.84L3.5 7.27l1.23-1.23 1.33 1.33 3.21-3.21L10.5 5.4 6.06 9.84z"></path></svg>',
+    verifi: "Payment verified",
+    spent: "$100k+",
+    location: " United States",
+  },
+];
